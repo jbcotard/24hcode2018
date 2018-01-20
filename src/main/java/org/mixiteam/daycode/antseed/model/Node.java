14a944e50fdde2a53eb5a76aa92963a46c23e394
user <user@pc>
2018-01-20 17:35:26 +0100

    Node

---
+++ src/main/java/org/mixiteam/daycode/antseed/model/Node.java
@@ -0,0 +1,51 @@
+public class Node
+{
+    Position position;
+    int temps_attente;
+    int temps_passe;
+    int vitesse;
+
+    public Node()
+    {
+    }
+
+    public void setPosition(Position p)
+    {
+        this.position = p;
+    }
+
+    public void setTempsAttente(int v)
+    {
+        this.temps_attente = v;
+    }
+
+    public void setTempsPasse(int v)
+    {
+        this.temps_passe = v;
+    }
+
+    public void setVitesse(int v)
+    {
+        this.vitesse = v;
+    }
+
+    public Position getPosition()
+    {
+        return position;
+    }
+
+    public int getTempsAttente()
+    {
+        return temps_attente;
+    }
+
+    public int getTempsPasse()
+    {
+        return temps_passe;
+    }
+
+    public int getVitesse()
+    {
+        return vitesse;
+    }
+}
