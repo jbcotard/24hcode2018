2118ce293429f6cebface83b587d81e45ebba9e8
jb <jb.cotard@laposte.net>
2018-01-20 17:58:28 +0100

    add getter position

--- src/main/java/org/mixiteam/daycode/antseed/Position.java
+++ src/main/java/org/mixiteam/daycode/antseed/Position.java
@@ -4,4 +4,25 @@ public class Position {
 
     String lat;
     String lon;
+
+    public Position(String lat, String lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "lat='" + lat + '\'' +
+                ", lon='" + lon + '\'' +
+                '}';
+    }
 }
