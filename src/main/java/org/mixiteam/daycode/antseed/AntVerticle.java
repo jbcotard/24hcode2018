1893ab073a84e254e0b52ceb356da2f85b14035f
jb <jb.cotard@laposte.net>
2018-01-20 17:59:35 +0100

    add api user me

--- src/main/java/org/mixiteam/daycode/antseed/AntVerticle.java
+++ src/main/java/org/mixiteam/daycode/antseed/AntVerticle.java
@@ -7,6 +7,8 @@ import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientRequest;
 import io.vertx.ext.unit.Async;
 
+import java.util.List;
+
 public class AntVerticle extends AbstractVerticle {
     @Override
     public void start(Future<Void> future) throws Exception {
@@ -15,85 +17,25 @@ public class AntVerticle extends AbstractVerticle {
 
         System.out.println("Traitement ant ");
 
-        // recuperation donnees graine
+        /* recuperation donnees graine */
 
-        HttpClient client = vertx.createHttpClient();
+        // authent fourmis
+        String token = ServeurAntSeed.auth("ant1@violet.ant", "Prune");
+        System.out.println("token : " + token);
 
-
-
-/*        HttpClientRequest request = client.post("https://f24h2018.herokuapp.com/auth/local", response -> {
-            System.out.println("Received response with status code " + response.statusCode());
-        });
-*/
-
-/*
-
-        HttpClientRequest request = client.postAbs("https://f24h2018.herokuapp.com/auth/local", response -> {
-            System.out.println("Received response with status code " + response.statusCode());
-            response.bodyHandler( body -> System.out.println("Got data " + body.toString("ISO-8859-1")));
-
-        });
-
-        request.putHeader("content-length", "1000");
-        request.putHeader("content-type", "text/plain");
-        request.write("email=ant1@violet.ant&password=Prune");
-        request.end();
-*/
-
-
-        //Async async = context.async();
-
-        //HttpClient client = vertx.createHttpClient();
-
-        HttpClientRequest request = client.post(443, "f24h2018.herokuapp.com", "/auth/local", response -> {
+        // recuperation data fourmis
+        Position positionFourmis = ServeurAntSeed.getUserMe(token);
+        System.out.println(positionFourmis.toString());
 
 
 
-            System.out.println("Some callback " + response.statusCode());
-
-            //async.complete();
+        /* calcul trajectoire */
 
 
-
-        });
-
-        String body = "{'email':'ant1@violet.ant','password':'Prune'}";
-
-        request.putHeader("content-length", "1000");
-
-        request.putHeader("content-type", "application/json");
-
-        request.write(body);
-
-        request.end();
+        Position positionGraine = null;
+        List<Position> listePositionsTrajet = TrajetCalculateur.calculerPositionsTrajet(positionFourmis, positionGraine);
 
 
-
-
-// Now do stuff with the request
-        //request.putHeader("content-length", "1000");
-        //request.putHeader("content-type", "text/plain");
-        //request.write("email=ant1@violet.ant&password=Prune");
-
-// Make sure the request is ended when you're done with it
-        //request.end();
-
-
-        // definition zone à partir position graine
-
-
-        // recuperation des zones
-
-
-        // calcul du graphe des prositions
-
-
-        // calcul de la ponderation des chemins
-
-
-        // creation des positions
-
-        // creation du parcours
 
     }
 }
--- src/main/java/org/mixiteam/daycode/antseed/ServeurAntSeed.java
+++ src/main/java/org/mixiteam/daycode/antseed/ServeurAntSeed.java
@@ -1,5 +1,16 @@
 package org.mixiteam.daycode.antseed;
 
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.json.Json;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ServeurAntSeed {
 
     /**
@@ -8,13 +19,66 @@ public class ServeurAntSeed {
      * @param password
      * @return
      */
-    public static String auth(String email, String password) {
+    public static String auth(String email, String password) throws IOException {
 
         String token = null;
 
-        return token;
+        URL urlClient = new URL("https://f24h2018.herokuapp.com/auth/local");
+        Map<String,Object> params = new LinkedHashMap<>();
+        params.put("password", password);
+        params.put("email", email);
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String,Object> param : params.entrySet()) {
+            if (postData.length() != 0) postData.append('&');
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        HttpURLConnection conn = (HttpURLConnection) urlClient.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+        conn.setDoOutput(true);
+        conn.getOutputStream().write(postDataBytes);
+
+        System.out.println("response : " + conn.getResponseCode());
+
+        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+        StringBuilder tokenBuilder = new StringBuilder();
+        for (int c; (c = in.read()) >= 0;)
+            tokenBuilder.append((char)c);
+
+
+
+        return Json.decodeValue(tokenBuilder.toString(), Token.class).getToken();
     }
 
 
+    public static Position getUserMe(String token) throws IOException {
 
+
+        URL urlClient = new URL("https://f24h2018.herokuapp.com/api/users/me");
+
+
+        HttpURLConnection conn = (HttpURLConnection) urlClient.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Authorization", "Bearer " + token);
+        conn.setDoOutput(true);
+
+        System.out.println("response : " + conn.getResponseCode());
+
+        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+        StringBuilder tokenBuilder = new StringBuilder();
+        for (int c; (c = in.read()) >= 0;)
+            tokenBuilder.append((char)c);
+
+        Insecte fourmis = Json.decodeValue(tokenBuilder.toString(), Insecte.class);
+        Position positionFourmis = new Position(fourmis.getLat(), fourmis.getLon());
+
+        return positionFourmis;
+    }
 }
