53e0aa76a4234d87a9e2d76b2d2871bf1ac7f1c9
jb <jb.cotard@laposte.net>
2018-01-20 20:47:20 +0100

    ajout timestam dans position

--- src/main/java/org/mixiteam/daycode/antseed/Position.java
+++ src/main/java/org/mixiteam/daycode/antseed/Position.java
@@ -1,14 +1,23 @@
 package org.mixiteam.daycode.antseed;
 
+import java.time.ZonedDateTime;
+
 public class Position {
 
     String lat;
     String lon;
+    ZonedDateTime timestamp;
 
-    public Position(String lat, String lon) {
-        this.lat = lat;
-        this.lon = lon;
-    }
+    /*
+    * // works with Instant
+Instant instant = Instant.now();
+System.out.println(instant.format(DateTimeFormatter.ISO_INSTANT));
+
+// works with ZonedDateTime
+ZonedDateTime zdt = ZonedDateTime.now();
+System.out.println(zdt.format(DateTimeFormatter.ISO_INSTANT));
+    *
+    * */
 
     public String getLat() {
         return lat;
@@ -18,11 +27,16 @@ public class Position {
         return lon;
     }
 
+    public ZonedDateTime getTimestamp() {
+        return timestamp;
+    }
+
     @Override
     public String toString() {
         return "Position{" +
                 "lat='" + lat + '\'' +
                 ", lon='" + lon + '\'' +
+                ", timestamp=" + timestamp +
                 '}';
     }
 }
